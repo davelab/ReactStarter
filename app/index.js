4b74e5cbafd213f2d3fd08e2c19702dad4ef143f
Davide Cariani <davide.cariani@engrande.com>
2016-05-17 15:59:35 +0200

    webpack integration with hot reloading and sass support

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Components
-import App from './Components/App.jsx'
+import App from './Components/App'
 
 
 ReactDOM.render(<App />, document.getElementById('App'));
